19e52773851d7de9e0b49b4d957688b9873010b3
meerkenia <132111009+meerkenia@users.noreply.github.com>
2023-06-29 23:23:55 -0400

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,8 +1,8 @@
 def tip_calculator():
-    bill = float(input("How much is the restaurant bill?\n"))
+    bill = float(input("How much is the restaurant bill?\n$"))
     percent = float(input("What percent do you want to tip?\n"))
     tip = bill * (percent / 100)
     total_price = bill + tip
-    return total_price
+    return f"The total price is: ${round(total_price,2)}"
 
 print(tip_calculator())
